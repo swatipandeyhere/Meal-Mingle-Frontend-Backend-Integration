d76d0f62b53f052898c2c6e1bb5e73836e281a09
Swati Pandey <swatipandeyhere@gmail.com>
2024-06-10 18:15:59 +0530

    Fix Login UI - II

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -46,16 +46,12 @@ const Login = () => {
     }
     return (
         <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
             <div className="fixed inset-0 bg-black bg-opacity-85 transition-opacity"></div>
-
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-96 sm:max-w-lg">
                         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
-
                                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                     <div className='flex '>
                                         <h3 className="text-3xl font-semibold leading-6 text-gray-600" id="modal-title">Login</h3>
@@ -66,8 +62,7 @@ const Login = () => {
                                             country={'in'}
                                             value={phone}
                                             onChange={(phone) => setPhone("+" + phone)}
-                                            buttonStyle={{ backgroundColor: "white" }}
-                                        />
+                                            buttonStyle={{ backgroundColor: "white" }} />
                                     </div>
                                     <button onClick={sendOtp} className="mt-5 mb-3 bg-rose-500 w-full h-12 text-white py-2 px-4 rounded">
                                         Send One Time Password
@@ -81,12 +76,12 @@ const Login = () => {
                                         <div className='text-center mb-3'>or</div>
                                         <Link to='/emailLogin'><div className='flex items-center text-center border border-spacing-1 rounded-lg p-3'>
                                             <img src={EmailIcon} alt='Email Icon' className='w-7 h-7 ml-12' />
-                                            <button className='ml-5'>Continue with Email</button>
+                                            <button className='ml-2'>Continue with Email</button>
                                         </div>
                                         </Link>
                                         <div onClick={googleSignIn} className='mt-5 flex items-center text-center border border-spacing-1 rounded-lg p-3'>
                                             <img src={GoogleIcon} alt='Google Icon' className='w-7 h-7 ml-12' />
-                                            <button className='ml-5'>Continue with Google</button>
+                                            <button className='ml-2'>Continue with Google</button>
                                         </div>
                                     </div>}
                                     <hr className='mt-4' />
