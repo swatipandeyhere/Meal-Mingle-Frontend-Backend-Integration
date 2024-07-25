7f816cbc2b45a2c8ae6284755364aec0c7f8a819
Swati Pandey <swatipandeyhere@gmail.com>
2024-07-25 23:58:31 +0530

    Integrate MyOrders Component with Backend API

--- src/components/MyOrders.tsx
+++ src/components/MyOrders.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
+import { toast } from 'react-toastify';
 
 interface OrderItem {
-    restaurantItemId: string;
-    restaurantItemName: string;
-    restaurantItemPrice: number;
-    quantity: number;
-    restaurantItemImageUrl: string;
+    orderItemName: string;
+    orderItemQuantity: number;
+    orderItemPrice: number;
 }
 
 interface ShippingAddress {
-    streetNumber: string;
+    pincode: string;
     streetName: string;
     city: string;
     country: string;
@@ -18,16 +17,35 @@ interface ShippingAddress {
 
 interface Order {
     id: string;
-    items: OrderItem[];
-    totalAmount: number;
+    orderItems: OrderItem[];
+    orderTotalPrice: number;
     orderDate: string;
     orderTimestamp: number;
-    status: 'Pending' | 'Confirmed' | 'Preparing' | 'Ready' | 'Delivered';
+    orderStatus: 'Pending' | 'Confirmed' | 'Preparing' | 'Ready' | 'Delivered';
     shippingAddress: ShippingAddress;
 }
 
 const MyOrders = () => {
     const [orders, setOrders] = useState<Order[]>([]);
+
+    const token = localStorage.getItem('token');
+
+    const fetchOrders = async () => {
+        const response = await fetch('http://localhost:8093/api/orders', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        const data = await response.json();
+        console.log(data.data.order);
+        setOrders(data.data.order);
+    }
+
+    useEffect(() => {
+        fetchOrders();
+    }, []);
 
     useEffect(() => {
         const savedOrders = localStorage.getItem('orders');
@@ -63,12 +81,25 @@ const MyOrders = () => {
         return () => clearInterval(intervalId);
     }, []);
 
-    const cancelOrder = (orderId: string) => {
+    const cancelOrder = async (order: any) => {
         const isConfirmed = window.confirm('Are you sure you want to Cancel this Order?');
         if (isConfirmed) {
-            const updatedOrders = orders.filter(order => order.id !== orderId);
-            localStorage.setItem('orders', JSON.stringify(updatedOrders));
-            setOrders(updatedOrders);
+            const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:8093/api/orders/${order.orderId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            const data = await response.json();
+
+            if (data.error != "") {
+                toast.error(data.message);
+            }
+            else {
+                toast.success(data.message);
+                fetchOrders();
+            }
         }
     };
 
@@ -92,7 +123,7 @@ const MyOrders = () => {
             <div className='p-4 pl-20'>
                 <h1 className='text-3xl font-bold mb-4'>My Orders</h1>
                 {orders.length === 0 ? (
-                    <p>You have No Orders yet!</p>
+                    <p className="text-center text-xl mt-72">You have No Orders yet!</p>
                 ) : (
                     <div className='space-y-4'>
                         {orders.map((order) => {
@@ -101,32 +132,32 @@ const MyOrders = () => {
                                 <div key={order.id} className='border p-4 rounded shadow mr-10'>
                                     <h2 className='text-xl font-semibold mb-2'>Order ID: {order.id}</h2>
                                     <p className='text-gray-600 mb-2'>Order Date: {order.orderDate}</p>
-                                    <p className='text-gray-600 mb-2'>Status: {order.status}</p>
+                                    <p className='text-gray-600 mb-2'>Status: {order.orderStatus}</p>
                                     <div className='text-gray-600 mb-2'>
-                                        Shipping Address: {order.shippingAddress.streetNumber}, {order.shippingAddress.streetName}, {order.shippingAddress.city}, {order.shippingAddress.country}
+                                        Shipping Address: {order.shippingAddress.pincode}, {order.shippingAddress.streetName}, {order.shippingAddress.city}, {order.shippingAddress.country}
                                     </div>
                                     <div className='space-y-2'>
-                                        {order.items.map((item, index) => (
+                                        {order.orderItems.map((item, index) => (
                                             <div key={index} className='flex justify-between items-center p-2 border-b'>
                                                 <div className='flex flex-col'>
-                                                    <span className='font-semibold'>{item.restaurantItemName}</span>
-                                                    <span className='text-gray-500'>Qty: {item.quantity}</span>
+                                                    <span className='font-semibold'>{item.orderItemName}</span>
+                                                    <span className='text-gray-500'>Qty: {item.orderItemQuantity}</span>
                                                 </div>
                                                 <div className='flex flex-col items-end'>
-                                                    <span className='text-gray-500'>Price: ₹{item.restaurantItemPrice}</span>
-                                                    <span className='font-semibold'>Total: ₹{item.restaurantItemPrice * item.quantity}</span>
+                                                    <span className='text-gray-500'>Price: ₹{item.orderItemPrice}</span>
+                                                    <span className='font-semibold'>Total: ₹{item.orderItemPrice * item.orderItemQuantity}</span>
                                                 </div>
                                             </div>
                                         ))}
                                     </div>
-                                    <div className='font-semibold mt-2'>Total Amount: ₹{order.totalAmount}</div>
+                                    <div className='font-semibold mt-2'>Total Amount: ₹{order.orderTotalPrice}</div>
                                     {timeRemaining > 0 && (
                                         <div className='mt-2'>
                                             <p className='text-red-500'>
                                                 Time Left to Cancel: {formatTime(timeRemaining)}
                                             </p>
                                             <button
-                                                onClick={() => cancelOrder(order.id)}
+                                                onClick={() => cancelOrder(order)}
                                                 className='mt-2 px-4 py-2 bg-red-500 text-white rounded'
                                             >
                                                 Cancel Order
