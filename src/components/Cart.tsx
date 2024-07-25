e2e3fcc67f637c4a2544a454d41c5ad2363206ce
Swati Pandey <swatipandeyhere@gmail.com>
2024-07-25 15:55:17 +0530

    Update Cart Component Logic based on Backend

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -22,29 +22,19 @@ const Cart = () => {
         }
     };
 
-    const uniqueRestaurantId = cart.length > 0 ? cart[0].restaurantItemId.split('_')[0] : '';
-
-    const restaurant = restaurants.find((r) => r.restaurantId === uniqueRestaurantId);
-
+    let discountPercentage = cart.length > 0 ? Number(cart[0].restaurantDiscountPercentage) : 0;
     const totalPrice = calculateTotalPrice();
-    let discountedPrice = totalPrice;
-    let discountAmount = 0;
-
-    if (restaurant && restaurant.restaurantDiscountPercentage > 0 && totalPrice > restaurant.restaurantMinimumOrderAmount) {
-        const { discountedPrice: calculatedDiscountedPrice, discountAmount: calculatedDiscountAmount } = calculateDiscountedPrice(
-            totalPrice,
-            restaurant.restaurantMinimumOrderAmount,
-            restaurant.restaurantDiscountPercentage
-        );
-        discountedPrice = calculatedDiscountedPrice;
-        discountAmount = calculatedDiscountAmount;
-    }
+    let discounts = cart.length > 0 ? calculateDiscountedPrice(totalPrice,
+        Number(cart[0].restaurantMinimumOrderAmount), discountPercentage) : { discountedPrice: 0, discountAmount: 0 };
+    let discountedPrice = discounts.discountedPrice;
+    let discountedAmount = discounts.discountAmount;
 
     const proceedToPayment = () => {
-        if (cart.length > 0 && restaurant) {
+        if (cart.length > 0) {
             const paymentState = {
                 items: cart,
-                restaurant,
+                restaurantId: cart[0].restaurantId,
+                restaurantName: cart[0].restaurantName,
                 totalPrice: discountedPrice
             };
             localStorage.setItem('paymentState', JSON.stringify(paymentState));
@@ -58,13 +48,13 @@ const Cart = () => {
             <div className='p-4 pl-20'>
                 <h1 className='font-semibold text-3xl mb-4'>Your Cart</h1>
                 {cart.length === 0 ? (
-                    <p>Your Cart is Empty!</p>
+                    <p className="text-center text-xl mt-72">Your Cart is Empty!</p>
                 ) : (
                     <div>
                         <div className='grid grid-cols-3 gap-4'>
                             {cart.map((item: CartItem) => (
                                 <div key={item.id} className='max-w-xs rounded-xl overflow-hidden shadow-sm'>
-                                    <img className='w-full rounded-2xl h-60 object-cover' src={require(`../images/${item.restaurantItemImageUrl}`)} alt={item.restaurantItemName} />
+                                    <img className='w-full rounded-2xl h-60 object-cover' src={item.restaurantItemImageUrl} alt={item.restaurantItemName} />
                                     <div className='py-4'>
                                         <div className='flex justify-between items-center'>
                                             <div className='font-semibold text-xl mb-2'>{item.restaurantItemName}</div>
@@ -88,11 +78,11 @@ const Cart = () => {
                         </div>
                         <div className='mt-5'>
                             <p className='font-semibold'>Total Price: ₹{totalPrice}</p>
-                            {restaurant && restaurant.restaurantDiscountPercentage > 0 && totalPrice > restaurant.restaurantMinimumOrderAmount && (
+                            {cart && Number(cart[0].restaurantDiscountPercentage) > 0 && totalPrice > cart[0].restaurantMinimumOrderAmount && (
                                 <>
-                                    <p className='font-semibold'>Discount: {restaurant.restaurantDiscountPercentage}%</p>
-                                    <p className='font-semibold'>Discount Amount: ₹{discountAmount.toFixed(2)}</p>
-                                    <p className='font-semibold'>Discounted Price: ₹{discountedPrice.toFixed(2)}</p>
+                                    <p className='font-semibold'>Discount: {cart[0].restaurantDiscountPercentage}%</p>
+                                    <p className='font-semibold'>Discount Amount: ₹{Number(discountedAmount).toFixed(2)}</p>
+                                    <p className='font-semibold'>Discounted Price: ₹{Number(discountedPrice).toFixed(2)}</p>
                                 </>
                             )}
                             <button onClick={clearCart} className='mr-4 mt-5 px-4 py-2 bg-red-500 text-white rounded'>Clear Cart</button>
